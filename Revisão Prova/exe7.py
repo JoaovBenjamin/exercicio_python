74053f402572a54d1d667c4beb347a42f60d5435
Joaovvb <rm98938@fiap.com.br>
2023-05-02 00:29:00 -0300

    estudando

---
+++ Revisão Prova/exe7.py
@@ -0,0 +1,1 @@
+# Escreva um programa em Python que leia uma matriz M [5x5] e, a cada linha, multiplique cada elemento pelo valor do elemento inserido na diagonal principal da linha. Os elementos da matriz serão fornecidos pelo usuário. Escrever a matriz M lida e a matriz M modificada.
